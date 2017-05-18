f35cdc971c90aa8dc38c8d90c8147250fefa66ac
Danny <DJ@MacBook-Pro.local>
2017-05-18 14:29:31 -0600

    basic structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,39 @@
 var presidents = ['George Washington', 'Thomas Jefferson', 'James Madison','James Monroe','Andrew Jackson','Martin Van Buren', 'Zachary Taylor', 'Abraham Lincoln', 'Andrew Johnson','Theodore Roosevelt', 'Woodrow Wilson','John Kennedy','Richard Nixon','Gerald Ford','Jimmy Carter','Ronald Reagan','Bill Clinton','Herbert Hoover','Calvin Coolidge','Grover Cleveland'];
 
+var chooseWord = presidents[Math.floor(Math.random() * presidents.length)];
 
-function chooseWord (presidents) {
-
-    return presidents[Math.floor(Math.random() * presidents.length)];
     
-} 
+
 
 console.log("pick names working");
 
+var p;
+var guesses = 5;
+var answerArray = [];
 
-var wordLength = presidents.length;
-var underscores = '';
-for(i=0; i<wordLength; i++) {
-    underscores = underscores + "_ ";
+function startUp() {
 
+for (var i = 0; i < chooseWord.length; i++){
+	answerArray[i] = "_";
 }
-document.getElementById("demo").innerHTML = underscores;
-console.log(underscores);
+
+console.log("underscores");
+
+p = answerArray.join(" ");
+	document.getElementById("presidents").innerHTML = p;
+}
+console.log("array working");
+
+function Letter(){
+	var letter = document.getElementById("letter").value;
+	if (letter.length > 0){
+		for(var i = 0; i < chooseWord.length; i++)
+		{
+			if(chooseWord[i] === letter)
+			{
+				answerArray[i] = letter;
+			}
+		}
+	}
+}
 
